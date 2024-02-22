91799700b31d96d5962c8dbd0d443eb0380f201b
shoukat mulla <shoukatmulla741@gmail.com>
2024-02-22 23:31:32 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElem = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElem);
+
+// Function to handle the intersection changes
+// Function to check if an element is in the viewport
+// Function to handle the intersection changes
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+const boxes = document.querySelectorAll('.test');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+   const triggerBottom = (window.innerHeight / 5) * 4;
+
+   boxes.forEach((box) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+         box.classList.add('show');
+      } else {
+         box.classList.remove('show');
+      }
+   });
+}
+
+window.addEventListener("scroll", reveal);
+
+const boxes1 = document.querySelectorAll('.test1');
+
+window.addEventListener('scroll', checkBoxes1);
+
+checkBoxes1();
+
+function checkBoxes1() {
+   const triggerBottom = (window.innerHeight / 5) * 4;
+
+   boxes1.forEach((box) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+         box.classList.add('show');
+      } else {
+         box.classList.remove('show');
+      }
+   });
+}
+
+
+
